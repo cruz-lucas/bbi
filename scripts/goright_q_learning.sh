36e9e7da97692e1b049befdab5edfa23762c49a8
Lucas Cruz <lcruz1@narval3.narval.calcul.quebec>
2024-12-07 21:59:05 -0500

    build: add requirements file

--- scripts/goright_q_learning.sh
+++ scripts/goright_q_learning.sh
@@ -10,7 +10,7 @@ virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 
-pip install -e .
+pip install -r requirements.txt
 make q-learning
 
 deactivate
