3080439d95c57ec351c95571c5b7b1c326a073e9
Lucas Cruz <lcruz1@narval2.narval.calcul.quebec>
2024-12-12 16:37:11 -0500

    chore: update compute canada script

--- scripts/goright_q_learning.sh
+++ scripts/goright_q_learning.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1-00:00                       # Run for DD-HH:MM
 #SBATCH --job-name=goright_q_learning
 #SBATCH --output=%x-%j.out
 
-module load python/3.10 gcc arrow/17.0.0
+cd $SLURM_TMPDIR
+git clone git@github.com:cruz-lucas/bbi.git
+cd ./bbi
+
+module load python/3.10
+
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index --upgrade pip
 
-pip install -r requirements.txt
+pip install --no-index --upgrade pip
+pip install -r requirements.txt --no-index
+
+export $(xargs <$PROJECT/bbi/.env)
 make q-learning
 
 deactivate
